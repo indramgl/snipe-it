3089451ac7de08b42878b00cec92dc704b725b7e
indramgl <indramgl@users.noreply.github.com>
2024-07-01 15:10:11 +0700

    Update auth.php
    Added translation from English to Indonesia from line 16 to line 18.

--- resources/lang/id-ID/auth.php
+++ resources/lang/id-ID/auth.php
@@ -13,8 +13,8 @@ return array(
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Detail tersebut tidak ditemukan di catatan kami.',
+    'password' => 'Password yang diberikan salah.',
+    'throttle' => 'Terlalu banyak percobaan. Mohon tunggu kembali dalam :seconds seconds.',
 
 );
